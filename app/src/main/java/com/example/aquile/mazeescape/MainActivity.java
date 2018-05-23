9c0c28f269a61a9aede1bb21cce0a5e8fe3c71f1
Aquile-GH <pepijnkeizer@gmail.com>
2018-05-23 15:57:31 +0200

    -

--- app/src/main/java/com/example/aquile/mazeescape/MainActivity.java
+++ app/src/main/java/com/example/aquile/mazeescape/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aquile.mazeescape;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+
+    }
+
+    public void openHighScores(View view) {
+
     }
 }
