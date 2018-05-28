336f25657908102986f0342ac9c4d86b1a3be6ca
Aquile-GH <pepijnkeizer@gmail.com>
2018-05-28 15:31:41 +0200

    -

--- app/src/main/java/com/example/aquile/mazeescape/MainActivity.java
+++ app/src/main/java/com/example/aquile/mazeescape/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.aquile.mazeescape;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
