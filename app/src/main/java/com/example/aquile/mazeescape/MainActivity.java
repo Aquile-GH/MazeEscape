8405bfd48c7dd5b7dce7ea6cafe62a9d3b448ba6
Aquile-GH <pepijnkeizer@gmail.com>
2018-05-28 16:47:45 +0200

    -

--- app/src/main/java/com/example/aquile/mazeescape/MainActivity.java
+++ app/src/main/java/com/example/aquile/mazeescape/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.aquile.mazeescape;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-    }
+        }
 
     public void startGame(View view) {
         Intent newIntent = new Intent(MainActivity.this, MainGame.class);
